c5f7f4ea778ccd67a8260f72087d7e38acf1060f
Sean Jordan <jordansean10@gmail.com>
2020-06-29 23:03:20 -0700

    Added index.html

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const options = {
+        root: path.join(__dirname, 'index.html'),
+    };
+
+    res.sendFile('index.html', options, (error) => {
+        if (error) {
+            console.log('Failed to send index.html');
+        } else {
+            console.log('Sent index.html');
+        }
+    });
 })
 
 app.get('/christine', (req, res) => {
-    res.send('Hi Christine');
+    res.send('Hi Christine, I love you! :)');
 })
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`Now listening at http://localhost:${port}`));
